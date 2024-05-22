cfb6261a6c44b534f783eaeca78424ef5fbfd519
Chris Price <chris@momentohq.com>
2024-05-21 17:00:55 -0700

    WIP

---
+++ example/aws/cdk-lambda/infrastructure/lib/MomentoSimpleLambdaStack.ts
@@ -0,0 +1,35 @@
+import * as path from 'path';
+import * as cdk from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import * as secrets from 'aws-cdk-lib/aws-secretsmanager';
+import {RustFunction} from 'cargo-lambda-cdk';
+
+export class MomentoSimpleLambdaStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const momentoApiKeyParam = new cdk.CfnParameter(this, 'MomentoApiKey', {
+      type: 'String',
+      description: 'The Momento API key that will be used to read from the cache.',
+      noEcho: true,
+    });
+
+    const apiKeySecret = new secrets.Secret(this, 'MomentoSimpleGetApiKey', {
+      secretName: 'MomentoSimpleGetApiKey',
+      secretStringValue: new cdk.SecretValue(momentoApiKeyParam.valueAsString),
+    });
+
+    const rustLambda = new RustFunction(this, 'MomentoSimpleRustLambda', {
+      functionName: 'MomentoSimpleRustLambda',
+      runtime: 'provided.al2023',
+      manifestPath: path.join(__dirname, '../../lambda/momento-simple-lambda/Cargo.toml'),
+      timeout: cdk.Duration.seconds(30),
+      memorySize: 128,
+      environment: {
+        MOMENTO_API_KEY_SECRET_NAME: apiKeySecret.secretName,
+      },
+    });
+
+    apiKeySecret.grantRead(rustLambda);
+  }
+}
---
+++ example/aws/cdk-lambda/lambda/momento-simple-lambda/src/main.rs
@@ -0,0 +1,21 @@
+use serde_json::Value;
+use lambda_runtime::{run, service_fn, tracing, Error, LambdaEvent};
+
+
+/// This is the main body for the function.
+/// Write your code inside it.
+/// There are some code example in the following URLs:
+/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
+/// - https://github.com/aws-samples/serverless-rust-demo/
+async fn function_handler(event: LambdaEvent<Value>) -> Result<(), Error> {
+    // Extract some useful information from the request
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing::init_default_subscriber();
+
+    run(service_fn(function_handler)).await
+}
